54eaf96e5dba172e6e16b77d169d4def59d83b27
anqizhang <angela.cheung1026@gmail.com>
2022-05-02 20:53:32 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Anqi Zhang");
+        System.out.println("Java Web");
+        System.out.println("Omaha, NE");
+        System.out.println("Tiramisu");
+    }
+}
